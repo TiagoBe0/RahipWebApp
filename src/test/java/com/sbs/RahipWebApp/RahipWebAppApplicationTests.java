6cbcc91bd65fd0acfe3785804587c92f2e0250b4
TiagoBe0 <santiagobergamin@gmail.com>
2025-05-08 20:30:50 -0300

    f

---
+++ src/test/java/com/sbs/RahipWebApp/RahipWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.RahipWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahipWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
